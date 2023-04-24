0bc4826ab48031923344ba846d7d0419582e11b0
Mironova <marinamir2002@mail.ru>
2023-04-24 18:41:36 +0700

    add dz1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { Calculator } from './apps/calculator';
+import { Information } from './apps/calculator';
+
 
 function hello(user) {
   if (user) {
@@ -12,17 +14,26 @@ function hello(user) {
 }
 
 function App() {
-  const a = 10;
-  const b = 15;
+const a = 15;
+const b = 10;
 
   let user = {
-    name: 'John',
-    last_name: 'Barlowe',
-    age: 30,
-    cat: {
+    
+      name: 'John',
+      last_name: 'Barlowe',
+      age: 30,
+  
+    cat: [
+      {
         name: 'Happy',
         age: 1,
-    },
+      },
+      {
+        name: 'Tom',
+        age: 4,
+      }
+    ],
+    
     salaries: [
         {
           salary: '5670',
@@ -39,56 +50,56 @@ function App() {
     ],
   };
 
-  const c = <span>Hello</span>;
-  const name = hello();
+  
+ // const salaries = user.salaries.map((salary, index) => {
+ //   return <tr><td>{index + 1}</td><td>{salary.salary}</td><td>{salary.from_date}</td></tr>
+ // });
 
-  // reduce
-  // filter
-  // forEach
-  // map
-
-  // <tr><td>11</td><td>5670</td><td>2018</td></tr>
-  const salaries = user.salaries.map((salary, index) => {
-    return <tr><td>{index + 1}</td><td>{salary.salary}</td><td>{salary.from_date}</td></tr>
-  });
-
-  // comment
   return (
-    <div> 
-      <p>A: {a}</p>
-      <p>B: {b}</p>
+    <div>
+    {/* <p>Данные:</p> */}
+      {/* <p>Имя: {user.name}</p> */}
+      {/* <p>Фамилия: {user.last_name}</p> */}
+      {/* <p>Возраст: {user.age}</p> */}
 
-      <p>Sum: {a + b}</p>
-
-      <p>{c}, {name}</p>
-
-      {/* <p>{Calculator()}</p> */}
-      {/* <Calculator user={user} a={'text'} b={b}/> */}
-      <Calculator/>
-      <Calculator/>
-      <Calculator/>
-
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Salary</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {salaries}
-        </tbody>
-        {/* <tfoot>
-          <tr>
-            <td>#</td>
-            <td>Salary</td>
-            <td>Date</td>
-          </tr>
-        </tfoot> */}
-      </table>
+      {/* <table> */}
+      {/* <caption>Питомцы:</caption> */}
+        {/* <thead> */}
+          {/* <tr> */}
+            {/* <th>#</th> */}
+            {/* <th>Name</th> */}
+            {/* <th>Age</th> */}
+          {/* </tr> */}
+        {/* </thead> */}
+        {/* <tbody> */}
+          {/* {pets} */}
+        {/* </tbody> */}
+      {/* </table> */}
+      {/* <table> */}
+        {/* <caption>Зарплата:</caption> */}
+        {/* <thead> */}
+          {/* <tr> */}
+            {/* <th>#</th> */}
+            {/* <th>Salary</th> */}
+            {/* <th>Date</th> */}
+          {/* </tr> */}
+        {/* </thead> */}
+        {/* <tbody> */}
+          {/* {salaries} */}
+        {/* </tbody> */}
+      {/* </table> */}
+{/*  */}
+       {/* <p>{Calculator()}</p>   */}
+       {/* <Calculator user={user} a={'text'} b={b}/>   */}
+     {/*  */}
+        {/* {<Calculator/> }  */}
+        {/* {<Calculator/> }  */}
+        {/* {<Calculator/> }  */}
+       {/*  */}
+       {<Information user={user}/>}
     </div>
-  );
-}
+  ); 
+};
+
 
 export default App;
--- src/apps/calculator.js
+++ src/apps/calculator.js
@@ -8,5 +8,49 @@ export function Calculator(props) {
         <input type='text' placeholder="Text" onChange={(event) => {
             setText(event.target.value);
         }}/>
-    </>;
-}
+   </>;
+};
+export function Information(props) {
+    let pets = props.user.cat.map((cats, index) => {
+        return <tr><td>{index + 1}</td><td>{cats.name}</td><td>{cats.age}</td></tr>
+});
+    let salaries = props.user.salaries.map((salary, index) => {
+        return <tr><td>{index + 1}</td><td>{salary.salary}</td><td>{salary.from_date}</td></tr>
+      });
+   
+    return (
+    <div>
+    <p>Данные:</p>
+      <p>Имя: {props.user.name}</p>
+      <p>Фамилия: {props.user.last_name}</p>
+      <p>Возраст: {props.user.age}</p>
+
+      <table>
+      <caption>Питомцы:</caption>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pets}
+        </tbody>
+      </table>
+      <table>
+        <caption>Зарплата:</caption>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Salary</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {salaries}
+        </tbody>
+      </table>
+      </div>
+    );
+};
